c9241f78476fa078388e57c9d9025148e5261e23
harish_selva <97356861+Harishraj16@users.noreply.github.com>
2023-09-05 19:42:20 +0530

    Update 02. Second largest and Second Smallest.cpp

--- Practice/array/easy/02. Second largest and Second Smallest.cpp
+++ Practice/array/easy/02. Second largest and Second Smallest.cpp
@@ -1,3 +1,4 @@
+//Better Solution[O(n log n)]:
 #include<bits/stdc++.h>
 vector<int> getSecondOrderElements(int n, vector<int> a) {
     if (n < 2) {
@@ -12,3 +13,38 @@ vector<int> getSecondOrderElements(int n, vector<int> a) {
     return {secondLargest,secondSmallest};
 }
 
+
+//optimal Solution [O(n)]:
+
+#include <vector>
+
+std::vector<int> getSecondOrderElements(int n, std::vector<int> a) {
+    if (n < 2) {
+        // Handle the case where the vector has fewer than 2 elements.
+        // You can return an empty vector or handle it as needed.
+        return {};
+    }
+
+    int smallest = a[0];
+    int secondSmallest = INT_MAX;
+    int largest = a[0];
+    int secondLargest = INT_MIN;
+
+    for (int i = 1; i < n; i++) {
+        if (a[i] < smallest) {
+            secondSmallest = smallest;
+            smallest = a[i];
+        } else if (a[i] < secondSmallest && a[i] != smallest) {
+            secondSmallest = a[i];
+        }
+
+        if (a[i] > largest) {
+            secondLargest = largest;
+            largest = a[i];
+        } else if (a[i] > secondLargest && a[i] != largest) {
+            secondLargest = a[i];
+        }
+    }
+
+    return {secondSmallest, secondLargest};
+}
