8516cdc28a7a8d22e47d83e40641e9588a48c0cd
harish_selva <harishselvaraj27@gmail.com>
2023-09-15 19:14:13 +0530

    Create 01. Power set using Bit_manipulation.cpp

---
+++ Practice/Bit_Manipulation/01. Power set using Bit_manipulation.cpp
@@ -0,0 +1,17 @@
+vector<string> result;
+        int n = s.size();
+
+        for (int num = 1; num < (1 << n); num++) { // Start from 1 to exclude the empty string
+            string sub = "";
+            for (int i = 0; i < n; i++) {
+                if (num & (1 << i)) {
+                    sub += s[i];
+                }
+            }
+            result.push_back(sub);
+        }
+
+        sort(result.begin(), result.end()); // Sort the result vector
+
+        return result;
+    }
