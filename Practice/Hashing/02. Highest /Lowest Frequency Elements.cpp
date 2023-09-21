ac4059efdaad969720ceaf224d3a426dfd440873
harish_selva <harishselvaraj27@gmail.com>
2023-09-21 18:46:23 +0530

    Create  Lowest Frequency Elements.cpp

---
+++ Practice/Hashing/02. Highest /Lowest Frequency Elements.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+vector<int> getFrequencies(vector<int>& v) {
+    int n=v.size();
+
+    map<int,int>mpp;
+
+    for(int i=0;i<n;i++){
+
+        mpp[v[i]]++;
+
+    }
+
+    vector<int>freq(2);
+
+    int max=INT_MIN;
+
+    int min=INT_MAX;
+
+    for(auto i:mpp){
+
+        if(i.second>max){
+
+            freq[0]=i.first;
+
+            max=i.second;
+
+        }
+
+        if(i.second<min){
+
+            freq[1]=i.first;
+
+            min=i.second;
+
+        }
+
+    }
+
+    return freq;
+}
