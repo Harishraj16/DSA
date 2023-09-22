10ee083f9144de93cd0034e33e6796c2c510a8a3
harish_selva <harishselvaraj27@gmail.com>
2023-09-22 19:25:29 +0530

    Update 02. Bubble_sort.cpp

--- Practice/Sorting/02. Bubble_sort.cpp
+++ Practice/Sorting/02. Bubble_sort.cpp
@@ -1,4 +1,6 @@
- void bubbleSort(int arr[], int n)
+//Usual_solution:
+
+void bubbleSort(int arr[], int n)
     {
         for(int i=0;i<n-1;i++){
             for(int j=0;j<n-1-i;j++){
@@ -7,4 +9,19 @@
                 }
             }
         } 
+    }
+
+//Using recursion:
+
+ void bubbleSort(int arr[], int n)
+    {
+        if(n == 1){
+            return;
+        }
+            for(int j=0;j<n-1;j++){
+                if(arr[j]>arr[j+1]){
+                    swap(arr[j],arr[j+1]);
+                }
+            }
+        bubbleSort(arr,n-1);
     }
