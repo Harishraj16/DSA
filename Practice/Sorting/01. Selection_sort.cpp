4af9c7b57e41ed2dcbf2ad86b42987d7d03ba2b7
harish_selva <harishselvaraj27@gmail.com>
2023-09-22 19:10:00 +0530

    Update 01. Selection_sort.cpp

--- Practice/Sorting/01. Selection_sort.cpp
+++ Practice/Sorting/01. Selection_sort.cpp
@@ -1,3 +1,5 @@
+//Usual solution:
+
 void selectionSort(vector<int>&arr) {
    int n= arr.size();
    for(int i=0;i<n-1;i++){
@@ -12,3 +14,25 @@ void selectionSort(vector<int>&arr) {
        arr[i] = temp;
    }
 }
+
+
+//Using Recursion:
+
+int select(int arr[], int i)
+    {
+        int n=sizeof(arr)/sizeof(arr[0]);
+        if(i == n-1){
+            return 0;
+        }
+        
+        int mini = i;
+        
+        for(int j=i;j<n;j++){
+            if(arr[j]<arr[mini]){
+                mini = j;
+            }
+        }
+        swap(arr[i],arr[mini]);
+        
+        select(arr,i++);
+    }
