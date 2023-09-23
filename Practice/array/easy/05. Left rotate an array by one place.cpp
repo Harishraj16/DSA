625f8493e1a2e4af59bb92987ea7d0e2cf4d9a73
harish_selva <harishselvaraj27@gmail.com>
2023-09-23 19:33:31 +0530

    Update 05. Left rotate an array by one place.cpp

--- Practice/array/easy/05. Left rotate an array by one place.cpp
+++ Practice/array/easy/05. Left rotate an array by one place.cpp
@@ -7,3 +7,17 @@ vector<int> rotateArray(vector<int>& arr, int n) {
     arr[n-1]=temp;
     return arr;
 }
+
+//Leetcode (Rotate array to right by K steps):
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+      k=k%nums.size(); /* when the k value exceeds the size of nums[]  
+        eg Qn: rotate the array right to 8 steps but arr size is 7.
+        k= 8 % 7 -> k=1 now, you can understand the concept behind this */
+
+      reverse(nums.begin(),(nums.begin()+nums.size()-k));
+      reverse((nums.begin()+nums.size()-k),nums.end());
+      reverse(nums.begin(),nums.end());
+    }
+};
