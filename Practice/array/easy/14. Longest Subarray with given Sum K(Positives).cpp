e9c7eed091a5dde4826d558e0e1d8617d02144f9
harish_selva <harishselvaraj27@gmail.com>
2023-09-25 12:29:27 +0530

    Create 14. Longest Subarray with given Sum K(Positives).cpp

---
+++ Practice/array/easy/14. Longest Subarray with given Sum K(Positives).cpp
@@ -0,0 +1,21 @@
+//Brute force approach: time:  ~O(n^3)    space: O(1)
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+        for (int j = i; j < n; j++) { // ending index
+            // add all the elements of
+            // subarray = a[i...j]:
+            long long s = 0;
+            for (int K = i; K <= j; K++) {
+                s += a[K];
+            }
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
