ee809c05e86cae76bb1de139261358c30712e50b
harish_selva <harishselvaraj27@gmail.com>
2023-09-27 21:54:24 +0530

    Create 11. Superior Elements.cpp

---
+++ Practice/array/Medium/11. Superior Elements.cpp
@@ -0,0 +1,17 @@
+
+
+//Optimal Approach:  Time:O(N)  Space:O(N)
+
+vector<int> superiorElements(vector<int>&a) {
+    int n=a.size();
+    vector<int> ans;
+    int max=INT_MIN;
+    for(int i=n-1;i>=0;i--){
+        if(a[i]>max){
+            ans.push_back(a[i]);
+            max=a[i];
+        }
+    }
+    //sort(ans.begin(),ans.end());
+    return ans;
+}
