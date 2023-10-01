de43dff87d0d848817b07b696d4d01788dad6bae
harish_selva <harishselvaraj27@gmail.com>
2023-10-01 07:33:29 +0530

    Update 14. Longest successive element.cpp

--- Practice/array/Medium/14. Longest successive element.cpp
+++ Practice/array/Medium/14. Longest successive element.cpp
@@ -49,3 +49,28 @@ int longestSuccessiveElements(vector<int>&a) {
     }
     return longest;
 }
+
+//Optimal Approach 2: TIme:O(N)+O(2N)~O(3N)     Space:O(N) 
+
+int longestSuccessiveElements(vector<int>&a) {
+    int n=a.size();
+    if(n==0) return 0;
+    unordered_set<int> st;
+    for(int i=0;i<n;i++){
+        st.insert(a[i]);
+    }
+    
+    int longest=1;
+    for(auto it:a){
+        if(st.find(it-1)==st.end()){
+            int count=1;
+            int x=it;
+            while (st.find(x + 1) != st.end()) {
+              count++;
+              x++;
+            }
+            longest = max(longest, count);
+        }
+    }
+    return longest;
+}
