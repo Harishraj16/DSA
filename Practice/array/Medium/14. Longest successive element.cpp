ef2eb111164150ade71ab26bf5ffd55339554f32
harish_selva <harishselvaraj27@gmail.com>
2023-10-01 07:57:13 +0530

    Update 14. Longest successive element.cpp

--- Practice/array/Medium/14. Longest successive element.cpp
+++ Practice/array/Medium/14. Longest successive element.cpp
@@ -26,28 +26,43 @@ int longestSuccessiveElements(vector<int>&a) {
 
 //Optimal approach 1:  Time:O(NlogN)+O(N)  Space:O(1)
 
-bool linearsearch(vector<int>&a,int num){
-    for(int i=0;i<a.size();i++){
-        if(a[i]==num)
-            return true;
-    }
-    return false;
-}
+#include <bits/stdc++.h>
+using namespace std;
 
 int longestSuccessiveElements(vector<int>&a) {
-    int longest=1;
-    for(int i=0;i<a.size();i++){
-        int x=a[i];
-        int count=1;
-        for(int j=0;j<a.size();j++){
-            if(linearsearch(a,x+1)){
-                count++;
-                x++;
-            }
+    int n = a.size();
+    if (n == 0) return 0;
+
+    //sort the array:
+    sort(a.begin(), a.end());
+    int lastSmaller = INT_MIN;
+    int cnt = 0;
+    int longest = 1;
+
+    //find longest sequence:
+    for (int i = 0; i < n; i++) {
+        if (a[i] - 1 == lastSmaller) {
+            //a[i] is the next element of the
+            //current sequence.
+            cnt += 1;
+            lastSmaller = a[i];
         }
-        longest=max(longest,count);
+        else if (a[i] != lastSmaller) {
+            cnt = 1;
+            lastSmaller = a[i];
+        }
+        longest = max(longest, cnt);
     }
     return longest;
+
+}
+
+int main()
+{
+    vector<int> a = {100, 200, 1, 2, 3, 4};
+    int ans = longestSuccessiveElements(a);
+    cout << "The longest consecutive sequence is " << ans << "\n";
+    return 0;
 }
 
 //Optimal Approach 2: TIme:O(N)+O(2N)~O(3N)     Space:O(N) 
