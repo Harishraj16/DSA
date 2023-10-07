ea9058c87676e3dd8b70233c63e8dca0c1e90d51
harish_selva <harishselvaraj27@gmail.com>
2023-10-07 07:06:05 +0530

    Create 03. Implement Lower Bound.cpp

---
+++ Practice/Binary Search/03. Implement Lower Bound.cpp
@@ -0,0 +1,40 @@
+//Brute Force approach:  Time:O(N)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> arr, int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= x) {
+            // lower bound found:
+            return i;
+        }
+    }
+    return n;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int n = 5, x = 9;
+    int ind = lowerBound(arr, n, x);
+    cout << "The lower bound is the index: " << ind << "\n";
+    return 0;
+}
+
+
+
+//Optimal approach:  Time: O(log N)
+
+int lowerBound(vector<int> arr, int n, int x) {
+	int low=0,high=n-1,ans=n;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]>=x){
+			ans=mid;
+			high =  mid-1;
+		} 
+		else low = mid+1;
+	}
+	return ans;
+}
