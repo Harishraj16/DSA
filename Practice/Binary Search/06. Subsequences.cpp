76e2a81425ce11da24be5385e9f21737e2919a3c
Harishraj16 <harishselvaraj27@gmail.com>
2023-11-02 10:11:20 +0530

    Created Subsequences

---
+++ Practice/Binary Search/06. Subsequences.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void subseq(vector<int>&ds,int arr[],int n,int i){
+    if(i>=n){
+        cout<<"Subsequece:";
+        if(!ds.empty()){
+            for(auto it:ds){
+            cout<<it<<" ";
+            }
+        }
+        else cout<<"{ }";
+        cout<<endl;
+        return;
+    }
+    
+    //pick the particular index into the subsequence
+    ds.push_back(arr[i]);
+    subseq(ds,arr,n,i+1);
+    ds.pop_back();
+    
+    //Not pick the particular index into the subsequence
+    subseq(ds,arr,n,i+1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter the value(n): ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter "<<n<<" elements:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int> ds;
+    subseq(ds,arr,n,0);
+    return 0;
+}
