5eb2bf93234d22f98ecb635e7f42ece7d8986460
harish_selva <harishselvaraj27@gmail.com>
2023-11-02 06:57:24 +0530

    Update 04. String_Palindrome_check.cpp

--- Practice/Recursion_Series/04. String_Palindrome_check.cpp
+++ Practice/Recursion_Series/04. String_Palindrome_check.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool palin1(string str,int l,int r){
+bool palin1(string& str,int l,int r){
     if(l>=r)return true;
     if(str[l]!=str[r]) return false;
     palin1(str,l+1,r-1);
 }
 
-bool palin2(string str,int i,int n){
+bool palin2(string& str,int i,int n){
     if(i>=n/2) return true;
     if(str[i]!=str[n-i]) return false;
     palin2(str,i+1,n);
