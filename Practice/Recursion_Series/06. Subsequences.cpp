3dc9c44f190065ccbf41bbe798d2644d1f2b9325
harish_selva <harishselvaraj27@gmail.com>
2023-11-03 07:21:12 +0530

    Update 06. Subsequences.cpp

--- Practice/Recursion_Series/06. Subsequences.cpp
+++ Practice/Recursion_Series/06. Subsequences.cpp
@@ -27,12 +27,56 @@ int main(){
     int n;
     cout<<"Enter the value(n): ";
     cin>>n;
-    int arr[n];
+    int arr[n];    
     cout<<"Enter "<<n<<" elements:";
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
     vector<int> ds;
     subseq(ds,arr,n,0);
+    return 0;
+}
+
+
+
+
+
+//Optimal: by using only n
+#include<bits/stdc++.h>
+using namespace std;
+
+void subseq(vector<int>&ds,int arr[],int n){
+    if(n<=0){
+        cout<<"Subsequece:";
+        if(!ds.empty()){
+            for(auto it:ds){
+            cout<<it<<" ";
+            }
+        }
+        else cout<<"{ }";
+        cout<<endl;
+        return;
+    }
+    
+    //pick the particular index into the subsequence
+    ds.push_back(arr[n-1]);
+    subseq(ds,arr,n-1);
+    ds.pop_back();
+    
+    //Not pick the particular index into the subsequence
+    subseq(ds,arr,n-1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter the value(n): ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter "<<n<<" elements:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int> ds;
+    subseq(ds,arr,n);
     return 0;
 }
