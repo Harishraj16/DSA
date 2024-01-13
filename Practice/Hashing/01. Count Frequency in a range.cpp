546e215ed5b39117bc6d699e4230965b1f794ad2
harish_selva <harishselvaraj27@gmail.com>
2024-01-13 20:07:46 +0530

    Update 01. Count Frequency in a range.cpp

--- Practice/Hashing/01. Count Frequency in a range.cpp
+++ Practice/Hashing/01. Count Frequency in a range.cpp
@@ -3,7 +3,7 @@
 vector<int> countFrequency(int n, int x, vector<int> &nums){
     vector<int> freq(n,0);
     for(int i=0;i<n;i++){
-        freq[nums[i]-1]++;
+        freq[nums[i]-1]++ ;
     }
     return freq;
 }
