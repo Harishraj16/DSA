da1f17365651d2218b3f73d4c56dceee2fd1dc94
harish_selva <harishselvaraj27@gmail.com>
2024-01-18 22:53:30 +0530

    Update 01. Remove outermost Paranthesis.cpp

--- Practice/Strings/01. Remove outermost Paranthesis.cpp
+++ Practice/Strings/01. Remove outermost Paranthesis.cpp
@@ -1,1 +1,67 @@
+//optimal way 1:
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int open = 0, close = 0;
+        string res = "";
+        int start = 0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') open++;
+            else if(s[i]==')') close++;
+            if(open == close){
+                res += s.substr(start+1,i-start-1);
+                start = i+1;
+            }
+        }
+        return res;
+    }
+};
 
+
+//optimal way 2:
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans = "";
+        int opened = 0;
+        for(char c: s){
+            if(c == '(' && opened++ > 0){
+                ans+=c;
+            }
+            if(c == ')' && opened-- > 1){
+                ans+=c;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//optimal way 3:
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        std::string str = "";
+        int cnt = 0;
+        for(auto it : s)
+        {
+            if(it  == '(')
+            {
+                if(cnt > 0)
+                {
+                    str+='(';
+                }
+                cnt++;
+            }
+            else
+            {
+                if(cnt>1)
+                {
+                    str+=')';
+                }
+                cnt--;
+            }
+        }
+        return str;
+    }
+};
