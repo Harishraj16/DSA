0218140f8e7520a896c0cb9a021d99dd2382855c
harish_selva <harishselvaraj27@gmail.com>
2024-01-22 22:12:59 +0530

    Update _08.FindIndex.java

--- Clg_Placement/Java/_08.FindIndex.java
+++ Clg_Placement/Java/_08.FindIndex.java
@@ -11,3 +11,19 @@ class Solution
         return arr;
     }
 }
+
+//without extra variables left and right:
+class Solution
+{ 
+    // Function to find starting and end index 
+    static int[] findIndex(int a[], int n, int key) { 
+        int[] arr = new int[2];
+        arr[0]=-1;
+        arr[1]=-1;
+        for(int i=0;i<n;i++){
+            if(a[i]==key && arr[0]==-1) arr[0] = i;
+            if(a[n-i-1]==key && arr[1]==-1) arr[1]=n-i-1; 
+        }
+        return arr;
+    }
+}
