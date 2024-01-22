0c0ff60921d87e03a5f3b086be5879db7055f9ec
harish_selva <harishselvaraj27@gmail.com>
2024-01-22 20:53:44 +0530

    Update _06.SumOfArrayElements.cpp

--- Clg_Placement/C++/_06.SumOfArrayElements.cpp
+++ Clg_Placement/C++/_06.SumOfArrayElements.cpp
@@ -1,1 +1,12 @@
-
+class Solution{
+public:
+	// function to return sum of elements
+	// in an array of size n
+	int sum(int arr[], int n) {
+        int sum = 0;
+        for (int i=0;i<n;i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+};
