6819e2bc75d7471d2549cbf3ce733460ae6824b3
harish_selva <harishselvaraj27@gmail.com>
2024-01-22 20:56:25 +0530

    Create _07.PrintElementsOfTheAray.cpp

---
+++ Clg_Placement/C++/_07.PrintElementsOfTheAray.cpp
@@ -0,0 +1,9 @@
+class Solution{
+public:
+    //Just print the space seperated array elements
+	void printArray(int arr[], int n) {
+	    for(int i=0;i<n;i++){
+	        cout<<arr[i]<<" ";
+	    }
+	}
+};
