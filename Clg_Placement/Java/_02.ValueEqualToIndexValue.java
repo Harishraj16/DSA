47a30272019cfa92b94548336e93512832dd583e
harish_selva <harishselvaraj27@gmail.com>
2024-01-22 19:18:58 +0530

    Create _02.ValueEqualToIndexValue.java

---
+++ Clg_Placement/Java/_02.ValueEqualToIndexValue.java
@@ -0,0 +1,9 @@
+class Solution {
+    ArrayList<Integer> valueEqualToIndex(int arr[], int n) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(arr[i]==i+1) result.add(arr[i]);
+        }
+        return result;
+    }
+}
