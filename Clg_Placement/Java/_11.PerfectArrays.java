7ff46e93dcd24d0c554f10b9bd5961037081bad2
harish_selva <harishselvaraj27@gmail.com>
2024-01-23 11:36:39 +0530

    Create _11.PerfectArrays.java

---
+++ Clg_Placement/Java/_11.PerfectArrays.java
@@ -0,0 +1,11 @@
+class Solution{
+    
+    public boolean IsPerfect(int a[], int n)
+    {
+       for(int i=0;i<n/2;i++){
+           if(a[i]!=a[n-i-1]) return false;
+       }
+       return true;
+    }
+    
+}
