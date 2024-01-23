84acb771801202db61d9bf0224a1f1c7066bde7a
harish_selva <harishselvaraj27@gmail.com>
2024-01-23 20:53:01 +0530

    Update _13.PrintTheLeftElement.java

--- Clg_Placement/Java/_13.PrintTheLeftElement.java
+++ Clg_Placement/Java/_13.PrintTheLeftElement.java
@@ -3,8 +3,8 @@ class Solution {
     public long leftElement(long arr[], long n)
     {
         Arrays.sort(arr);
-        if(n%2 != 0) return arr[(int)n/2];  //Here n might be a long then n/2 returns long 
-        //so to overcome the improper index value so we make explicit type conversion.
+        if(n%2 != 0) return arr[(int)n/2];  //Here n might be a long type then n/2 returns long 
+        //so to overcome the improper index value we do explicit type conversion.
         else if(n%2 == 0) return arr[(int)(n/2)-1];
         return -1;
     }
