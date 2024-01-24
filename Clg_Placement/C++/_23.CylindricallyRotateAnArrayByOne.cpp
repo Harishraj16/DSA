57d28adcba12486a1b90cb4667edf4728cdabb3d
harish_selva <harishselvaraj27@gmail.com>
2024-01-24 22:29:13 +0530

    Update _23.CylindricallyRotateAnArrayByOne.cpp

--- Clg_Placement/C++/_23.CylindricallyRotateAnArrayByOne.cpp
+++ Clg_Placement/C++/_23.CylindricallyRotateAnArrayByOne.cpp
@@ -1,5 +1,12 @@
 void rotate(int arr[], int n)
 {
+    reverse(arr, arr + n - 1);
+    reverse(arr, arr + n);
+}
+
+//optimized way
+void rotate(int arr[], int n)
+{
     int temp = arr[n-1];
     for(int i=n-1;i>0;i--){
         arr[i]=arr[i-1];
