4e1652a893e418ff57a624ff1138e4239abd3f19
harish_selva <harishselvaraj27@gmail.com>
2024-01-25 12:43:41 +0530

    Update _45.ElementsInTheRange.cpp

--- Clg_Placement/C++/_45.ElementsInTheRange.cpp
+++ Clg_Placement/C++/_45.ElementsInTheRange.cpp
@@ -1,3 +1,4 @@
+//Using Unordered_map:
 class Solution{
 	public:
 	bool check_elements(int arr[], int n, int A, int B)
@@ -13,3 +14,26 @@ class Solution{
 		return true;
 	}
 };
+
+
+//Using bool array:
+bool check_elements(int arr[], int N, int A, int B) {
+    // Initialize a boolean array to mark the presence of elements in the given range
+    bool present[B - A + 1] = {false};
+
+    // Traverse the array and mark elements in the given range
+    for (int i = 0; i < N; i++) {
+        if (arr[i] >= A && arr[i] <= B) {
+            present[arr[i] - A] = true;
+        }
+    }
+
+    // Check if all elements in the range are present
+    for (int i = 0; i <= B - A; i++) {
+        if (!present[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
