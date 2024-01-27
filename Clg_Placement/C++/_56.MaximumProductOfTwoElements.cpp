8291d9e04a21a8a149b3916af9a8b4bfe4a309d0
harish_selva <harishselvaraj27@gmail.com>
2024-01-27 11:48:33 +0530

    Update _56.MaximumProductOfTwoElements.cpp

--- Clg_Placement/C++/_56.MaximumProductOfTwoElements.cpp
+++ Clg_Placement/C++/_56.MaximumProductOfTwoElements.cpp
@@ -1,8 +1,34 @@
+//better solution: O(NlogN)
 class Solution{
 public:
 
 	int maxProduct(int arr[], int n) {
 	    sort(arr,arr+n);
 	    return arr[n-1]*arr[n-2];
+	}
+};
+
+
+//Optimal solution:   Time:O(N)
+class Solution{
+public:
+
+	int maxProduct(int arr[], int n) {
+	    int greatest = INT_MIN;
+	    int secondgreatest = INT_MIN;
+	    for(int i=0;i<n;i++){
+	        if(arr[i]>greatest){
+	            secondgreatest = greatest;
+	            greatest = arr[i];
+	        }
+	        else if(arr[i]>secondgreatest){   
+	            /* in else if dont do like
+               (arr[i]>secondgreatest && arr[i]!=greatest)
+               because same two numbers(duplicates) can be 
+               multiplied to get greatest sum*/
+	            secondgreatest = arr[i];
+	        }
+	    }
+	    return greatest*secondgreatest;
 	}
 };
