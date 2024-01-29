4b096f2974f74d780fc5964021f1961bc5a3687b
harish_selva <harishselvaraj27@gmail.com>
2024-01-29 19:32:39 +0530

    Update _23.CylindricallyRotateAnArrayByOne.cpp

--- Clg_Placement/Java/_23.CylindricallyRotateAnArrayByOne.cpp
+++ Clg_Placement/Java/_23.CylindricallyRotateAnArrayByOne.cpp
@@ -1,15 +1,11 @@
-void rotate(int arr[], int n)
-{
-    reverse(arr, arr + n - 1);
-    reverse(arr, arr + n);
-}
-
-//optimized way
-void rotate(int arr[], int n)
-{
-    int temp = arr[n-1];
-    for(int i=n-1;i>0;i--){
-        arr[i]=arr[i-1];
+class Compute {
+    
+    public void rotate(int arr[], int n)
+    {
+        int temp = arr[n-1];
+        for(int i=n-1;i>0;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=temp;
     }
-    arr[0]=temp;
 }
