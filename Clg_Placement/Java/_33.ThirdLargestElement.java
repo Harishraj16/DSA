3d6fed20483bd21343046db0f56f1b40408c236d
harish_selva <harishselvaraj27@gmail.com>
2024-01-30 18:43:53 +0530

    Update _33.ThirdLargestElement.java

--- Clg_Placement/Java/_33.ThirdLargestElement.java
+++ Clg_Placement/Java/_33.ThirdLargestElement.java
@@ -1,8 +1,36 @@
+//Brute Approach: time:O(NlogN)
 class Solution
 {
     int thirdLargest(int a[], int n)
     {
 	    Arrays.sort(a);
 	    return (int)a[n-3];
+    }
+}
+
+//Optimal Approach: time:O(N)
+class Solution
+{
+    int thirdLargest(int a[], int n)
+    {
+        int firstl = Integer.MIN_VALUE;
+        int secondl = Integer.MIN_VALUE;
+        int thirdl = Integer.MIN_VALUE;
+        
+        for(int i=0;i<n;i++){
+            if(a[i]>firstl){
+                thirdl = secondl;
+                secondl = firstl;
+                firstl = a[i];
+            }
+            else if(a[i]>secondl && a[i]<firstl){
+                thirdl = secondl;
+                secondl = a[i];
+            }
+            else if(a[i]>thirdl && a[i]<secondl){
+                thirdl = a[i];
+            }
+        }
+        return thirdl;
     }
 }
