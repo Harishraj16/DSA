57a7cbd4b13badf280adc29de33781236b8e672a
harish_selva <harishselvaraj27@gmail.com>
2024-01-31 07:08:10 +0530

    Update _43.GameWithNos.cpp

--- Clg_Placement/Java/_43.GameWithNos.cpp
+++ Clg_Placement/Java/_43.GameWithNos.cpp
@@ -1,7 +1,13 @@
-int* game_with_number(int arr[], int n)
-{
-   for(int i=0;i<n-1;i++){
-       arr[i] = arr[i]^arr[i+1];
-   }
-   return arr;
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int[] game_with_number (int arr[], int n) {
+        for(int i=0;i<n-1;i++){
+            arr[i] = arr[i]^arr[i+1];
+        }
+        return arr;
+    }
+    
+    
 }
