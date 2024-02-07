f9b151677e0b9a285b4d75cc70d5ac4ea80edce0
harish_selva <harishselvaraj27@gmail.com>
2024-02-07 19:27:34 +0530

    Update _17.SplitStrings.cpp

--- Clg_Placement/C++/Strings/_17.SplitStrings.cpp
+++ Clg_Placement/C++/Strings/_17.SplitStrings.cpp
@@ -5,6 +5,23 @@ public:
     { 
         vector<string> res(3,"");
         for(char &c: S){
+            if((c>=65 && c<=90) || (c>=97 && c<=122)) res[0]+=c;
+            else if(c>=48 && c<=57) res[1]+=c;
+            else res[2]+=c;
+        }
+        return res;
+    } 
+};
+
+
+
+class Solution
+{   
+public:
+    vector<string> splitString(string S) 
+    { 
+        vector<string> res(3,"");
+        for(char &c: S){
             if(isalpha(c)) res[0]+=c;
             else if(isdigit(c)) res[1]+=c;
             else res[2]+=c;
