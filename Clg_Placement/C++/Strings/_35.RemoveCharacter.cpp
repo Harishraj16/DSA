5bfe4906d4c280f382e817611764e5cae71d6fae
harish_selva <harishselvaraj27@gmail.com>
2024-02-08 19:26:15 +0530

    Update _35.RemoveCharacter.cpp

--- Clg_Placement/C++/Strings/_35.RemoveCharacter.cpp
+++ Clg_Placement/C++/Strings/_35.RemoveCharacter.cpp
@@ -1,3 +1,4 @@
+//Brute force: time:O(n*n)
 class Solution {
   public:
     string removeChars(string s1, string s2) {
@@ -9,6 +10,23 @@ class Solution {
             }
             res += s1[i];
             next: ;
+        }
+        return res;
+    }
+};
+
+//Better: time: O(N)   space:O(N)
+class Solution {
+  public:
+    string removeChars(string s1, string s2) {
+        string res="";
+        unordered_map<char,int> mpp;
+        for(char &c: s2){
+            mpp[c]++;
+        }
+        for(int i=0;i<s1.length();i++){
+            if(mpp.find(s1[i])!=mpp.end()) continue;
+            res+=s1[i];
         }
         return res;
     }
