b541c497c25d5c55771159252bed2ef778cbea78
harish_selva <harishselvaraj27@gmail.com>
2024-02-12 18:35:14 +0530

    Update _49.FindTheFIne.java

--- Clg_Placement/Java/_49.FindTheFIne.java
+++ Clg_Placement/Java/_49.FindTheFIne.java
@@ -16,3 +16,19 @@ class Solution {
         return amt;
     }
 }
+
+//Another way:
+class Solution {
+    
+    public long totalFine( long n, long date, long arr[], long fine[])
+    {
+        long amt = 0;
+        boolean flag = true;
+        if(date%2 != 0) flag = !flag;
+        for(int i=0;i<n;i++){
+            if(flag && arr[i]%2!=0) amt += fine[i];
+            else if((!flag) && arr[i]%2==0) amt += fine[i];
+        }
+        return amt;
+    }
+}
