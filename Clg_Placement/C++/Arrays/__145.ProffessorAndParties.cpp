f21375c23dfea18f809905aa9b1713f4ed02315c
harish_selva <harishselvaraj27@gmail.com>
2024-03-02 11:56:12 +0530

    Update __145.ProffessorAndParties.cpp

--- Clg_Placement/C++/Arrays/__145.ProffessorAndParties.cpp
+++ Clg_Placement/C++/Arrays/__145.ProffessorAndParties.cpp
@@ -13,3 +13,17 @@ class Solution{
         return "GIRLS";
     }
 };
+
+class Solution{
+    public:
+    
+    string PartyType(long long int a[], int n)
+    {
+        set<long long int> s;
+        for(int i=0;i<n;i++){
+            s.insert(a[i]);
+        }
+        if(s.size()==n)return "GIRLS";
+        return "BOYS";
+    }
+};
