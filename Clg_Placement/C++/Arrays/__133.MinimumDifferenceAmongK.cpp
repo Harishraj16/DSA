0a30ea7fee0421cae930ee610dbb97aff23768b2
harish_selva <harishselvaraj27@gmail.com>
2024-03-04 17:18:34 +0530

    Update __133.MinimumDifferenceAmongK.cpp

--- Clg_Placement/C++/Arrays/__133.MinimumDifferenceAmongK.cpp
+++ Clg_Placement/C++/Arrays/__133.MinimumDifferenceAmongK.cpp
@@ -1,3 +1,24 @@
+//Brute: O(nlogn)+O(n*k)
+class Solution{
+  public:
+    int minDiff(int arr[], int N, int K)
+    {
+        sort(arr,arr+N);
+        int result=INT_MAX;
+        for(int j=0;j<=N-K;j++){
+            int mini = INT_MAX;
+            int maxi = INT_MIN;
+            for(int i=j;i<j+K;i++){
+                mini = min(arr[i],mini);
+                maxi = max(arr[i],maxi);
+            }
+            result = min(result,maxi-mini);
+        }
+        return result;
+    }
+};
+
+//Optimized way: O(nlogn)
 class Solution{
   public:
     int minDiff(int arr[], int N, int K)
