d7eaf73719e2bd526c077e079eddda163cf75b2c
harish_selva <harishselvaraj27@gmail.com>
2024-03-06 19:43:14 +0530

    Update __114.AverageCountArray.java

--- Clg_Placement/Java/__114.AverageCountArray.java
+++ Clg_Placement/Java/__114.AverageCountArray.java
@@ -1,3 +1,22 @@
+//Brute Way:
+public static ArrayList<Integer> countArray (int arr[], int n, int x) {
+        //Complete the function
+        ArrayList<Integer> al = new ArrayList<Integer> ();
+        for(int i=0;i<n;i++){
+            int avg = (arr[i]+x)/2;
+            int count =0;
+            for(int j=0;j<n;j++){
+                if(arr[j]==avg){
+                    count++;
+                }
+            }
+          al.add(count);  
+        }
+        return al;
+    }
+
+
+//Better way:
 class Solution{
     // Function for finding maximum and value pair
     public static ArrayList<Integer> countArray (int arr[], int n, int x) {
