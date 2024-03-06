646ee9519e8b83623bacef0d2de7801042e65304
harish_selva <harishselvaraj27@gmail.com>
2024-03-06 17:09:03 +0530

    Update __120.MinimumProduct.java

--- Clg_Placement/Java/__120.MinimumProduct.java
+++ Clg_Placement/Java/__120.MinimumProduct.java
@@ -26,3 +26,21 @@ class Solution {
         return result;
     }
 }
+
+
+//MOST OPTIMAL WAY:
+class Solution {
+    public long printMinimumProduct(long arr[], long n)
+    {
+        long min1 = Long.MAX_VALUE;
+        long min2 = Long.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(arr[i]<min1){
+                min2=min1;
+                min1=arr[i];
+            }
+            else if (arr[i]>=min1 && arr[i]<min2) min2=arr[i];
+        }
+        return min1*min2;
+    }
+}
