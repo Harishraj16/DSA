8a68af679c4de11824accc165da1931bb590d2d6
harish_selva <harishselvaraj27@gmail.com>
2024-03-06 17:22:45 +0530

    Update __119.SmallestNumberRepeatingKTimes.java

--- Clg_Placement/Java/__119.SmallestNumberRepeatingKTimes.java
+++ Clg_Placement/Java/__119.SmallestNumberRepeatingKTimes.java
@@ -1,3 +1,4 @@
+//Using TreeMap:
 class Solution
 {
     int findDuplicate(int arr[], int N,int K)
@@ -14,5 +15,34 @@ class Solution
             }
         }
         return result;
+    }
+}
+
+//Using HashMap:
+class Solution
+{
+    int findDuplicate(int arr[], int N,int K)
+    {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<N;i++)
+        {
+            int key=arr[i];
+            int freq=hm.getOrDefault(key,0);
+            hm.put(key,freq+1);
+        }
+        //--------------
+        int ans=Integer.MAX_VALUE;
+        for(Map.Entry<Integer,Integer> e:hm.entrySet())
+        {
+            int key=e.getKey();
+            int val=e.getValue();
+            
+            if(val==K && key<ans)
+            {
+                ans=key;
+            }
+        }
+        if(ans==Integer.MAX_VALUE) return -1;
+        else return ans;
     }
 }
