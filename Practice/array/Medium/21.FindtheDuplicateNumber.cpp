902186e60b4277f05a56c933fb3741eb580f8720
harish_selva <harishselvaraj27@gmail.com>
2024-03-08 18:16:46 +0530

    Create 21.FindtheDuplicateNumber.cpp

---
+++ Practice/array/Medium/21.FindtheDuplicateNumber.cpp
@@ -0,0 +1,12 @@
+//287.Find the Duplicate Number
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            int index = abs(nums[i]);
+            if (nums[index] < 0) return index;
+            nums[index] *= -1;
+        }
+        return -1;
+    }
+};
