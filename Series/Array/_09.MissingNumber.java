a0ed0cf8cf1769dfcf51192be3a43af57bc29342
harish_selva <harishselvaraj27@gmail.com>
2024-03-09 19:30:25 +0530

    Create _09.MissingNumber.java

---
+++ Series/Array/_09.MissingNumber.java
@@ -0,0 +1,25 @@
+//268. Missing Number
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+        }
+        int target = n*(n+1)/2;
+        return target-sum;
+    }
+}
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor1 = 0;
+        int xor2 = 0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            xor2 ^= nums[i];
+            xor1 ^= (i+1);
+        }
+        return xor1^xor2;
+    }
+}
