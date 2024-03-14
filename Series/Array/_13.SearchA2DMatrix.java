4fffa5733d07b4738afdde9a19d3f4c904e6999d
harish_selva <harishselvaraj27@gmail.com>
2024-03-14 18:56:06 +0530

    Update _13.SearchA2DMatrix.java

--- Series/Array/_13.SearchA2DMatrix.java
+++ Series/Array/_13.SearchA2DMatrix.java
@@ -12,3 +12,28 @@ class Solution {
         return false;
     }
 }
+
+//Optimal way: 
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        if(target<matrix[0][0] || target>matrix[n-1][m-1]) return false;
+        int row = 0;
+        while(row<n){
+            if(target<= matrix[row][m-1]){
+                int left = 0;
+                int right = m-1;
+                while(left<=right){
+                    int mid = left+(right-left)/2;
+                    if(target == matrix[row][mid]) return true;
+                    else if(target<matrix[row][mid]) right = mid-1;
+                    else left = mid+1;
+                }
+                return false;
+            }
+            else row++;
+        }
+        return false;
+    }
+}
