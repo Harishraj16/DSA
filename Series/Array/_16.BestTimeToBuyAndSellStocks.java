68ba07b1c9a3bc579ea5335ad2e7d081d881eb04
harish_selva <harishselvaraj27@gmail.com>
2024-03-15 18:36:39 +0530

    Create _16.BestTimeToBuyAndSellStocks.java

---
+++ Series/Array/_16.BestTimeToBuyAndSellStocks.java
@@ -0,0 +1,13 @@
+//121. Best Time to Buy and Sell Stock
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int start = prices[0];
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<start) start = prices[i];
+            profit  = Math.max(profit,prices[i]-start);
+        }
+        return profit;
+    }
+}
