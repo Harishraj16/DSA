4acc189b4103bc53d9ee45285562b6232eca379e
harish_selva <harishselvaraj27@gmail.com>
2024-04-03 18:20:21 +0530

    Create _01.MaximumPointsYouCanObtainFromCards.java

---
+++ Practice/Sliding_Window_And_Two_Pointer/_01.MaximumPointsYouCanObtainFromCards.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxScore(int[] nums, int k) {
+        int lsum = 0,rsum = 0;
+        for(int i=0;i<k;i++){
+            lsum += nums[i];
+        }   
+        int maxsum = lsum;
+        int rind = nums.length-1;
+        for(int i = k-1;i>=0;i--){
+            lsum -= nums[i];
+            rsum += nums[rind--];
+            maxsum = Math.max(maxsum,lsum+rsum);
+        }
+        return maxsum;
+    }
+}
