3e1cc5857fbc18af0672df6f5d98ea0cd9a2cf8c
harish_selva <harishselvaraj27@gmail.com>
2024-04-05 19:04:52 +0530

    Update _02.MaxConsecutiveOnesIII.java

--- Practice/Sliding_Window_And_Two_Pointer/_02.MaxConsecutiveOnesIII.java
+++ Practice/Sliding_Window_And_Two_Pointer/_02.MaxConsecutiveOnesIII.java
@@ -16,3 +16,27 @@ class Solution {
         return maxlen;
     }
 }
+
+
+
+//Time: O(2n)  Space:O(1)
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length;
+        int l=0,r=0;
+        int zero = 0;
+        int maxlen = 0;
+        while(r<n){
+            if(nums[r]==0) zero++;
+            if(zero>k){
+                while(zero>k){
+                    if(nums[l]==0) zero--;
+                    l++;
+                }
+            }
+            maxlen = Math.max(maxlen,r-l+1);
+            r++;
+        }
+        return maxlen;
+    }
+}
