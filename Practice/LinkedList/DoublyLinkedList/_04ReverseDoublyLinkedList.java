f01705c560ee9b49523a3601ad9da6767bbf220c
harish_selva <harishselvaraj27@gmail.com>
2024-06-28 19:32:29 +0530

    Update _04ReverseDoublyLinkedList.java

--- Practice/LinkedList/DoublyLinkedList/_04ReverseDoublyLinkedList.java
+++ Practice/LinkedList/DoublyLinkedList/_04ReverseDoublyLinkedList.java
@@ -16,3 +16,21 @@ public static Node reverseDLL(Node  head)
     }
     return head;
 }
+
+
+//Optimal way
+//Time Comp: O(n)  Space Comp: O(1)
+
+public static Node reverseDLL(Node  head)
+{
+    if(head==null || head.next==null) return head;
+    Node prev = null;
+    Node curr = head;
+    while(curr!=null){
+        prev = curr.prev;
+        curr.prev = curr.next;
+        curr.next = prev;
+        curr = curr.prev;
+    }
+    return prev.prev;
+}
