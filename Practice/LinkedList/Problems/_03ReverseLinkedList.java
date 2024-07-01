8342109afe0fc72fa82926c70131eb5b79fb991a
harish_selva <harishselvaraj27@gmail.com>
2024-07-01 23:22:31 +0530

    Update _03ReverseLinkedList.java

--- Practice/LinkedList/Problems/_03ReverseLinkedList.java
+++ Practice/LinkedList/Problems/_03ReverseLinkedList.java
@@ -34,3 +34,16 @@ class Solution {
         return prev;
     }
 }
+
+
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode newHead = reverseList(head.next);
+        ListNode next = head.next;
+        next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
